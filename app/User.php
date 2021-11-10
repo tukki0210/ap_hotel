24afe55fc22cb0d07e759e50d201ef27a0f9b68c
tukki0210 <tukki0210@gmail.com>
2021-11-10 16:30:40 +0900

    整頓

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    protected $fillable = ['name','address','tel'];
-
     protected $primaryKey = 'user_id';
+
+    protected $fillable = ['name','address','tel'];
 }
